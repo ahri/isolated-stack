ad5df3f17da22e71a8618cd76af4e7af1545ddea
Adam Piper <adam@ahri.net>
2016-11-15 21:18:12 +0000

    Fix minor bug by using cd only in a subshell when building image.

--- stack.sh
+++ stack.sh
@@ -8,8 +8,7 @@ img=isolated-stack
 
 if ! docker images | awk -vimg=$img 'BEGIN { ret=1 } NR > 1 && $1 == img { ret=0 } END { exit ret }'; then
 	fullname="`readlink -e "$0"`"
-	cd "`dirname "$fullname"`"
-	docker build . -t $img
+	(cd "`dirname "$fullname"`" && docker build . -t $img)
 fi
 
 mkdir -p "$HOME/.stack" "$HOME/.local/bin"
